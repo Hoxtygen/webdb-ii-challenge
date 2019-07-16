00a54268eebe895acfca43ca2b1ebcd67241a4eb
Hoxtygen <hoxtygen@live.com>
2019-07-16 13:24:24 +0100

    Get all cars

--- server.js
+++ server.js
@@ -7,4 +7,26 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
 
+function getAllCars() {
+    return db('cars');
+  }
+
+
+
+
+
+
+
+
+  server.get('/', (req, res) => {
+    return res.status(200).json({
+        message: 'Welcome to Lambda Car dealership company'
+    })
+})
+
+server.get('/cars', async(req, res) => {
+  const cars = await getAllCars();
+  return res.json(cars);
+});
+
 module.exports = server;
